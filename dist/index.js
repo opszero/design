702146c11a312174707c841a3e12314af5abffce
Abhi Yerra <abhi@opszero.com>
2025-09-05 14:31:44 -0700

    Added index

--- dist/index.js
+++ dist/index.js
@@ -59,7 +59,7 @@ function copyrightFooter() {
                 <div class="container">
                     <div class="row" style="align-items: center; text-align: center;">
                         <div class="col col--4">
-                            <img src="/img/common/opsZero-Logo-Large.webp" alt="opsZero Logo" style="max-width: 200px; height: auto;" />
+                            <img src="https://opszero.com/img/common/opsZero-Logo-Large.webp" alt="opsZero" style="max-width: 200px; height: auto;" />
                         </div>
                         <div class="col col--4">
                             <p>©2016–${year} opsZero, LLC </p>
--- src/index.tsx
+++ src/index.tsx
@@ -61,7 +61,7 @@ export function copyrightFooter(): string {
                 <div class="container">
                     <div class="row" style="align-items: center; text-align: center;">
                         <div class="col col--4">
-                            <img src="/img/common/opsZero-Logo-Large.webp" alt="opsZero Logo" style="max-width: 200px; height: auto;" />
+                            <img src="https://opszero.com/img/common/opsZero-Logo-Large.webp" alt="opsZero" style="max-width: 200px; height: auto;" />
                         </div>
                         <div class="col col--4">
                             <p>©2016–${year} opsZero, LLC </p>
